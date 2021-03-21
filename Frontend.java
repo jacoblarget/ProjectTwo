f78b0eebf895e218a92445f1bcf0ce3bdb0c23db
Guilhem Ane <gane@wisc.edu>
2021-03-21 19:26:52 +0000

    Implemented the gen select, type select, and name select modes

--- Frontend.java
+++ Frontend.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
+import java.util.List;
 
 public class Frontend{
     //m(enu) g(eneration), t(ype), n(ame), p(ower), (e)x(it)
-    private String mode = "";
     private BackendInterface backend;
     private Scanner in;
 
@@ -26,8 +26,8 @@ public class Frontend{
         + "n : name search mode                                  '-----------'\n"
         + "p : total power filtering mode\n"
         + "x : exit\n");
-            mode = in.next();
-            switch(mode){
+            String input = in.next();
+            switch(input){
                 case "g":
                     runGenSelectMode();
                     break;
@@ -55,13 +55,30 @@ public class Frontend{
             printPokedexTitle();
             System.out.println(
               "Type the index of a generation you    .---------------------------.\n"
-            + "would like to select or deselect      | Generation Selection Mode |\n"
-            + "x : back to main menu                 '---------------------------'\n");
-            mode = in.next();
-            switch(mode){
-                case "x":
+            + "would like to select or deselect.     | Generation Selection Mode |\n"
+            + "Only one generation may be selected   '---------------------------'\n"
+            + "at a time. \n\n"
+            + "x : back to menu \n");
+
+            List<Integer> selectedGens = backend.getGenerations();
+            List<Integer> allGens = backend.getAllGenerations();
+            for(Integer gen : allGens){
+                System.out.println(
+                    "GENERATION "+gen+" : " + (selectedGens.contains(gen) ? "SELECTED" : ""));
+            }
+
+            String input = in.next();
+            try{
+                Integer selectedGen = Integer.parseInt(input);
+                if(selectedGens.contains(selectedGen)){
+                    backend.removeGeneration(selectedGen);
+                }else{
+                    backend.addGeneration(selectedGen);
+                }
+            }catch(NumberFormatException ex){
+                if(input.equals("x")){
                     isGenMenuRunning = false;
-                    return;
+                }
             }
         }
     }
@@ -74,11 +91,30 @@ public class Frontend{
               "Type the index of a type you would like     .---------------------.\n"
             + "to select or deselect                       | Type Selection Mode |\n"
             + "x : back to main menu                       '---------------------'\n");
-            mode = in.next();
-            switch(mode){
-                case "x":
+            
+            List<String> selectedTypes = backend.getTypes();
+            List<String> allTypes = backend.getAllTypes();
+            for(String type : allTypes){
+                System.out.println(
+                    type.toUpperCase() +" : " + (selectedTypes.contains(type) ? "SELECTED" : ""));
+            }
+
+            String input = in.next();
+
+            try{
+                Integer selectedTypeIndex = Integer.parseInt(input);
+                if(selectedTypeIndex < allTypes.size() && selectedTypeIndex >= 0){
+                    String selectedType = allTypes.get(selectedTypeIndex);
+                    if(selectedTypes.contains(selectedType)){
+                        backend.removeType(selectedType);
+                    }else{
+                        backend.addType(selectedType);
+                    }
+                }
+            }catch(NumberFormatException ex){
+                if(input.equals("x")){
                     isTypeMenuRunning = false;
-                    return;
+                }
             }
         }
     }
@@ -91,11 +127,19 @@ public class Frontend{
               "Type a name or partial name to search       .---------------------.\n"
             + "through the filtered Pokedex                | Name Selection Mode |\n"
             + "x : back to main menu                       '---------------------'\n");
-            mode = in.next();
-            switch(mode){
+            String input = in.next();
+            switch(input){
                 case "x":
                     isNameMenuRunning = false;
                     return;
+                default:
+                    PokemonInterface pokemon = backend.getByName(input);
+                    if(pokemon == null){
+                        System.out.println("No pokemon with the name "+input+" found.");
+                    }else{
+                        System.out.println(pokemon);
+                    }
+
             }
         }
     }
@@ -105,14 +149,29 @@ public class Frontend{
         while(isPowerMenuRunning){
             printPokedexTitle();
             System.out.println(
-              "Type the index of a power range you        .----------------------.\n"
-            + "would like to select or deselect           | Power Filtering Mode |\n"
-            + "x : back to main menu                      '----------------------'\n");
-            mode = in.next();
-            switch(mode){
-                case "x":
-                    isPowerMenuRunning = false;
-                    return;
+              "Type the minimum value the hundreds digit .----------------------.\n"
+            + "of total power you want.                  | Power Filtering Mode |\n"
+            + "For example, `2` will select the power    '----------------------'\n"
+            + "ranges from 200+.\n"
+            + "x : back to menu\n\n");
+
+            System.out.println("SELECTED POWER RANGES : ");
+            List<Integer> selectedPowers = backend.getPowers();
+            for(Integer range : selectedPowers){
+                System.out.println("\t"+(range*100) + " -> "+(range*100+100));
+            }
+            String input = in.next();
+            try{
+                Integer selectedPower = Integer.parseInt(input);
+                if(selectedPowers.contains(selectedPower)){
+                    backend.removeGeneration(selectedPower);
+                }else{
+                    backend.addGeneration(selectedPower);
+                }
+            }catch(NumberFormatException ex){
+                if(input.equals("x")){
+                    isGenMenuRunning = false;
+                }
             }
         }
     }
@@ -135,8 +194,8 @@ public class Frontend{
     }
 
     void consoleClear() {  
-        //System.out.print("\033[H\033[2J");
-        //System.out.flush();  
-    }  
-
+        try{
+            Runtime.getRuntime().exec("clear");
+        } catch (Exception e) { System.out.println(e); }
+    }
 }
